4d37e4d9589e81213895edc0fd13c155339344f6
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-08-28 23:19:31 +0800

    booking api - get

--- booking/src/main/java/com/booking/BookingController.java
+++ booking/src/main/java/com/booking/BookingController.java
@@ -1,15 +1,20 @@
-package com.booking.booking;
+package com.booking;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.booking.booking.Booking;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List; 
 
 @RestController
+@RequestMapping("/api/v1/booking")
 public class BookingController {
     @GetMapping
-    public List<Booking> hello() {
+    public List<Booking> getBookings() {
         return List.of(
                 new Booking(1L, "John Doe", "1234567890", LocalDate.of(2000, 01, 05), LocalTime.now(), "First booking",
                         "Confirmed"));
